77eb605c164168cdbead7fcc09d21856abd959d2
ricardoeuan <ricardo.euan@hotmail.com>
2016-04-26 18:45:26 -0500

    CM demo

---
+++ Examen2/app/src/main/java/com/example/ricardoeuanromo/examen2/Service/LocationService.java
@@ -0,0 +1,30 @@
+package com.example.ricardoeuanromo.examen2.Service;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by ricardoeuanromo on 4/4/16.
+ */
+public class LocationService implements LocationListener {
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
